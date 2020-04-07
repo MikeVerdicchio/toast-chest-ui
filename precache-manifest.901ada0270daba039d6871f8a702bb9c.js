cad400bdde6fc2676c00bc1802c6fc0bf6faa34c
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-07 13:50:09 -0400

    Updates

---
+++ precache-manifest.901ada0270daba039d6871f8a702bb9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6141654d81996db5c4c430e0f38f2a55",
+    "url": "/toast-chest-ui/index.html"
+  },
+  {
+    "revision": "db1c2dbcc35912f7e167",
+    "url": "/toast-chest-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f270144ef1278383f86",
+    "url": "/toast-chest-ui/static/js/2.6e17a8bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toast-chest-ui/static/js/2.6e17a8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1c2dbcc35912f7e167",
+    "url": "/toast-chest-ui/static/js/main.ca2db80a.chunk.js"
+  },
+  {
+    "revision": "0583cb7cc92f897cd2d8",
+    "url": "/toast-chest-ui/static/js/runtime-main.2b7990f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toast-chest-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
