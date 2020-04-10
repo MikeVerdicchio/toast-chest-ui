825ba4ee337efa202d0d89c57766fc32db69f52e
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-10 19:35:07 -0400

    Remove slash

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // Toast Chest URLs
-export const TOAST_CHEST_BASE_URL = 'https://toast-chest.herokuapp.com/';
+export const TOAST_CHEST_BASE_URL = 'https://toast-chest.herokuapp.com';
 export const TOAST_CHEST_RANDOM = `${TOAST_CHEST_BASE_URL}/random.json`;
 
 // Default toast to show on the homepage
