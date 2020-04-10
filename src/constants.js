8402a7b9e9bea73e2ce7497b888ecf8a8cacfc94
Mike Verdicchio <MikeVerdicchio@users.noreply.github.com>
2020-04-10 19:33:21 -0400

    Add toast component (#3)

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// Toast Chest URLs
+export const TOAST_CHEST_BASE_URL = 'https://toast-chest.herokuapp.com/';
+export const TOAST_CHEST_RANDOM = `${TOAST_CHEST_BASE_URL}/random.json`;
+
+// Default toast to show on the homepage
+export const DEFAULT_TOAST = "Here's to friendship, here's to great times, and here's to when the Toast Chest isn't around.";
