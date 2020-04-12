e77216e9ef5a4c161d3d6b738489ccecd0957276
Mike Verdicchio <MikeVerdicchio@users.noreply.github.com>
2020-04-11 22:30:33 -0400

    Sync ESLint and Prettier (#6)
    * Update configuration
    
    * Update pre-commit hook
    
    * Remove unused service worker
    
    * Prettier everything

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Toast from '../Toast';
+import "./App.css";
+import Toast from "../Toast";
 
 function App() {
   return (
--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   DEFAULT_TOAST,
   TOAST_CHEST_HEALTH,
   TOAST_CHEST_RANDOM,
-} from '../../constants';
-import './Toast.css';
+} from "../../constants";
+import "./Toast.css";
 
 function Toast() {
+  const [toast, setToast] = useState(null);
   const loadNewToast = async () => {
     const resp = await fetch(TOAST_CHEST_RANDOM);
     const data = await resp.json();
     setToast(data.toast);
   };
-
-  const [toast, setToast] = useState(null);
   const handleClick = () => loadNewToast();
 
   // Ping Toast Chest API on initial render to warm it up,
@@ -25,8 +24,12 @@ function Toast() {
   return (
     <div>
       <p className="Toast-text">{toast || DEFAULT_TOAST}</p>
-      <button className="btn btn-light btn-sm" onClick={handleClick}>
-        {toast ? 'Another!' : 'Generate a toast!'}
+      <button
+        className="btn btn-light btn-sm"
+        type="submit"
+        onClick={handleClick}
+      >
+        {toast ? "Another!" : "Generate a toast!"}
       </button>
     </div>
   );
