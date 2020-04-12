1c4b8cf34796c62c6ec827a5d6906a9d79432553
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-11 20:03:42 -0400

    Switch to React hooks

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
-import './Toast.css';
+import React, { useState, useEffect } from "react";
+import {
+    DEFAULT_TOAST,
+    TOAST_CHEST_HEALTH,
+    TOAST_CHEST_RANDOM
+} from "../../constants";
+import "./Toast.css";
 
-export default class Toast extends Component {
-    render() {
-        return (
-            <div>
-                <p className="Toast-text">{this.props.toast}</p>
-            </div>
-        );
-    }
+function Toast() {
+    const [toast, setToast] = useState(null);
+    const handleClick = () => loadNewToast();
+
+    // Ping Toast Chest API on initial render to warm it up,
+    // since free Heroku dynos sleep after 30 minutes.
+    useEffect(() => {
+        fetch(TOAST_CHEST_HEALTH);
+    }, []);
+
+    const loadNewToast = async () => {
+        const resp = await fetch(TOAST_CHEST_RANDOM);
+        const data = await resp.json();
+        setToast(data.toast);
+    };
+
+    return (
+        <div>
+            <p className="Toast-text">{toast || DEFAULT_TOAST}</p>
+            <button className="btn btn-light btn-sm" onClick={handleClick}>
+                {toast ? "Another!" : "Generate a toast!"}
+            </button>
+        </div>
+    );
 }
+
+export default Toast;
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 // Toast Chest URLs
-export const TOAST_CHEST_BASE_URL = 'https://toast-chest.herokuapp.com';
+export const TOAST_CHEST_BASE_URL = "https://toast-chest.herokuapp.com";
+export const TOAST_CHEST_HEALTH = `${TOAST_CHEST_BASE_URL}/health`;
 export const TOAST_CHEST_RANDOM = `${TOAST_CHEST_BASE_URL}/random.json`;
 
 // Default toast to show on the homepage
-export const DEFAULT_TOAST = "Here's to friendship, here's to great times, and here's to when the Toast Chest isn't around.";
+export const DEFAULT_TOAST =
+    "Here's to friendship, here's to great times, and here's to when the Toast Chest isn't around.";
