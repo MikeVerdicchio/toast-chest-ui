0df3413f289c73a1199830d712c6dcd8680f4610
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-11 20:16:15 -0400

    Run prettier

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-    DEFAULT_TOAST,
-    TOAST_CHEST_HEALTH,
-    TOAST_CHEST_RANDOM
-} from "../../constants";
-import "./Toast.css";
+  DEFAULT_TOAST,
+  TOAST_CHEST_HEALTH,
+  TOAST_CHEST_RANDOM,
+} from '../../constants';
+import './Toast.css';
 
 function Toast() {
-    const [toast, setToast] = useState(null);
-    const handleClick = () => loadNewToast();
+  const loadNewToast = async () => {
+    const resp = await fetch(TOAST_CHEST_RANDOM);
+    const data = await resp.json();
+    setToast(data.toast);
+  };
 
-    // Ping Toast Chest API on initial render to warm it up,
-    // since free Heroku dynos sleep after 30 minutes.
-    useEffect(() => {
-        fetch(TOAST_CHEST_HEALTH);
-    }, []);
+  const [toast, setToast] = useState(null);
+  const handleClick = () => loadNewToast();
 
-    const loadNewToast = async () => {
-        const resp = await fetch(TOAST_CHEST_RANDOM);
-        const data = await resp.json();
-        setToast(data.toast);
-    };
+  // Ping Toast Chest API on initial render to warm it up,
+  // since free Heroku dynos sleep after 30 minutes.
+  useEffect(() => {
+    fetch(TOAST_CHEST_HEALTH);
+  }, []);
 
-    return (
-        <div>
-            <p className="Toast-text">{toast || DEFAULT_TOAST}</p>
-            <button className="btn btn-light btn-sm" onClick={handleClick}>
-                {toast ? "Another!" : "Generate a toast!"}
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <p className="Toast-text">{toast || DEFAULT_TOAST}</p>
+      <button className="btn btn-light btn-sm" onClick={handleClick}>
+        {toast ? 'Another!' : 'Generate a toast!'}
+      </button>
+    </div>
+  );
 }
 
 export default Toast;
